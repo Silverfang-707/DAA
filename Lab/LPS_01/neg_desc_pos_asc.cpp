0bf9213736f837b32123489713ca2e996416c94f
Ramsundar <112865253+Silverfang707@users.noreply.github.com>
2023-05-22 21:31:13 +0530

    Create neg_desc_pos_asc.cpp

---
+++ Lab/LPS_01/neg_desc_pos_asc.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int> &elements, int n) {  
+    for(int j=1;j<n;j++)
+    {
+        int key = elements[j];
+        int i = j-1;
+        while((i>=0)&&(elements[i]>key))
+        {
+            elements[i+1] = elements[i];
+            i = i-1;
+        }
+        elements[i+1] = key;
+    }
+}
+
+void arrangeNumbers(vector<int> &numbers) {
+    vector<int> negatives;
+    vector<int> positives;
+
+    // Separate negative and positive numbers (including zero)
+    for (int num : numbers) {
+        if (num < 0)
+            negatives.push_back(num);
+        else
+            positives.push_back(num);
+    }
+
+    // Sort negative numbers in descending order
+    insertionSort(negatives, negatives.size());
+
+    // Sort positive numbers in ascending order
+    insertionSort(positives, positives.size());
+
+    // Concatenate the negative numbers, zero, and positive numbers
+    numbers.clear();
+    numbers.insert(numbers.end(), negatives.begin(), negatives.end());
+    numbers.insert(numbers.end(), positives.begin(), positives.end());
+}
+
+void numPush(vector<int> &numbers, int num){
+    int ele;
+    for(int i = 0; i < num;i++){
+        cout <<"Enter a Number: ";
+        cin >> ele;
+        numbers.push_back(ele);
+    }
+}
+
+int main() {
+    vector<int> numbers;
+    int n;
+    cout <<"Enter the number of elements: ";
+    cin >> n;
+    numPush(numbers, n);
+    arrangeNumbers(numbers);
+
+    // Print the arranged sequence
+    cout <<"Arranged sequence: "<< endl;
+    for (int num : numbers)
+        cout << num << " ";
+    cout << endl;
+
+    return 0;
+}
